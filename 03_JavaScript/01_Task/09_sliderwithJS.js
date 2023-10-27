5ee3909c5ab3ec111715672764c625c616cc9c8a
Niddhibhalala <nbhalara6874@gmail.com>
2023-10-27 17:24:34 +0530

    data

---
+++ 03_JavaScript/01_Task/09_sliderwithJS.js
@@ -0,0 +1,13 @@
+let slide(n) = document.getElementsByClassName("banner")
+let counter = 0
+function showslider() {
+    for (let i = 0; i < slider.length; i++) {
+        Slider[i].style.display = "none"
+    }
+    slider[counter].style.display = "flex"
+    counter++;
+    console.log("called", counter);
+    setTimeout(slider, 2000);
+
+}
+slider()
